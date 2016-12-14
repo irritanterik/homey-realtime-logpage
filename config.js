94ba37e7817df951b3b1e8f4b31326bbc5bfe2f5
gdvandongen <gdvandongen@gmail.com>
2016-12-14 22:38:40 +0100

    separated setup file

---
+++ config.js
@@ -0,0 +1,6 @@
+setup = {
+  // This can have nested stuff, arrays, etc.
+  ip: '0.0.0.0', // Put your ip here or athom cloud uri,
+  bearer_token: 'your token', // Put your bearer token here
+  protocol: 'http' // Change to https if using athom cloud
+}
