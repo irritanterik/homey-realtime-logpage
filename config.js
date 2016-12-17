c9f74f5cfee3320b106e56c42fbe6d49859e25ce
Erik van Dongen <irritanterik@users.noreply.github.com>
2016-12-17 18:40:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 setup = {
-  // This can have nested stuff, arrays, etc.
   ip: '0.0.0.0', // Put your ip here or athom cloud uri,
   bearer_token: 'your token', // Put your bearer token here
   protocol: 'http' // Change to https if using athom cloud
