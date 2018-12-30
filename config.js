647f795f36c5b5a851d750f4de9f1bc6e747282d
Erik van Dongen <gdvandongen@gmail.com>
2018-12-30 20:36:40 +0100

    Flow and card testing V2 compatible
    Added extra configuration option for HomeyId

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 setup = {
-  ip: '0.0.0.0', // Put your ip here or athom cloud uri,
-  bearer_token: 'your token', // Put your bearer token here
-  protocol: 'http' // Change to https if using athom cloud
+  ip: '0.0.0.0',            // Put your ip here or athom cloud uri,
+  bearer_token: 'your token',  // Put your v2  token here (144 characters long)
+  protocol: 'http',      // Change to https if using athom cloud
+  homeyId: 'your Homey ID'        // Homey ID
 }
