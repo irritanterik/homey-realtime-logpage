972c9a2bf761c8246a29ecf4d6e7e8e38ae8332f
Erik van Dongen <irritanterik@users.noreply.github.com>
2019-02-10 21:13:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 setup = {
   ip: '',            // Put your ip here or athom cloud uri,
-  bearer_token: '',  // Put your v2  token here (144 characters long)
+  bearer_token: '',  // Put your v2  token here (114 characters long)
   protocol: 'http',      // Change to https if using athom cloud
   homeyId: ''        // Homey ID
 }
